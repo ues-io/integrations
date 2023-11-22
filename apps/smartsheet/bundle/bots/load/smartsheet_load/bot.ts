c56f2e585ba7c78c9012bb0fe226a0e88b6b3bd9
Zach McElrath <zach@ues.io>
2023-11-22 07:16:44 -0500

    feat(smartsheet): Ability to filter for specific sheet row (#32)
    * feat(smartsheet): Add support for pagination and condition on rows
    * fix(smartsheet): Fix Pagination and add more tests

--- apps/smartsheet/bundle/bots/load/smartsheet_load/bot.ts
+++ apps/smartsheet/bundle/bots/load/smartsheet_load/bot.ts
@@ -21,10 +21,44 @@ type SheetResponse = {
 }
 
 export default function smartsheet_load(bot: LoadBotApi) {
-	const { collectionMetadata } = bot.loadRequest
-	const url =
-		"https://api.smartsheet.com/2.0/sheets/" +
-		collectionMetadata.externalName
+	const {
+		batchNumber = 0,
+		batchSize,
+		collectionMetadata,
+		conditions,
+	} = bot.loadRequest
+	const queryParams = {} as Record<string, FieldValue>
+	const doPagination = typeof batchSize === "number" && batchSize > 0
+	if (doPagination) {
+		// SmartSheet pagination uses 1-based indexing
+		queryParams.page = batchNumber + 1
+		// Always add one to to the batch size to determine if there are more records
+		queryParams.pageSize = batchSize + 1
+	} else {
+		queryParams.includeAll = true
+	}
+
+	// Transform Conditions on the Uesio Core Id into row Id filters
+	const rowIdCondition = conditions?.find(
+		(condition) => condition.field === "uesio/core.id"
+	)
+	if (rowIdCondition) {
+		if (rowIdCondition.operator === "IN" && rowIdCondition.values?.length) {
+			queryParams.rowIds = rowIdCondition.values.join(",")
+		} else if (rowIdCondition.operator === "EQ" && rowIdCondition.value) {
+			queryParams.rowIds = rowIdCondition.value
+		}
+	}
+
+	const queryString = Object.entries(queryParams)
+		.map(
+			([key, value]) =>
+				`${encodeURIComponent(key)}=${encodeURIComponent(`${value}`)}`
+		)
+		.join("&")
+
+	const url = `https://api.smartsheet.com/2.0/sheets/${collectionMetadata.externalName}?${queryString}`
+
 	const response = bot.http.request({
 		method: "GET",
 		url,
@@ -39,7 +73,17 @@ export default function smartsheet_load(bot: LoadBotApi) {
 		}
 	})
 
-	body.rows.forEach((row) => {
+	const maxRecordForPagination = doPagination
+		? (queryParams.pageSize as number) - 1
+		: -1
+	body.rows.forEach((row, i) => {
+		// If we are on the final row, do NOT add it to the bot's records,
+		// since we requested one more than we needed, but DO set has more records
+		// so that Uesio pagination controls will work
+		if (doPagination && i === maxRecordForPagination) {
+			bot.setHasMoreRecords()
+			return
+		}
 		const record: Record<string, FieldValue> = {
 			"uesio/core.id": row.id + "",
 		}
