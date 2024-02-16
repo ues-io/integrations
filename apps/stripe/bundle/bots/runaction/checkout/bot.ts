379ad6c6954f19fc2df8964445db21c2f33ad3b1
Abel Jimenez Molla <abel@ues.io>
2024-02-16 10:00:58 +0100

    fix bot

--- apps/stripe/bundle/bots/runaction/checkout/bot.ts
+++ apps/stripe/bundle/bots/runaction/checkout/bot.ts
@@ -15,36 +15,25 @@ export default function checkout(bot: RunActionBotApi) {
 		return
 	}
 
-	const { apikey } = bot.getCredentials()
-
-	if (apikey === "") {
-		bot.addError("missing api key")
-		return
-	}
-
 	const baseURL = bot.getIntegration().getBaseURL()
 
 	bot.log.info("ITEMS", items)
-	bot.log.info("apikey", apikey)
 	bot.log.info("baseURL", baseURL)
 	bot.log.info("mode", mode)
 	bot.log.info("successURL", successURL)
 
 	const result = bot.http.request({
 		method: "POST",
-		headers: {
-			Authorization: `Bearer ${apikey}`,
-		},
 		url: baseURL + "/v1/checkout/sessions",
 		body: {
 			mode,
 			success_url: successURL,
 		},
 	})
-	// if (result.code !== 200) {
-	// 	bot.addError("could not complete checkout: " + result.status)
-	// 	return
-	// }
+	if (result.code !== 200) {
+		bot.addError("could not complete checkout: " + result.status)
+		return
+	}
 
 	bot.log.info("Result", result)
 	bot.log.info("Body", result.body)
