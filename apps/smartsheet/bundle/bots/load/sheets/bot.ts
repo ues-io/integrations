4d1df1c22314313c39fd9b5b0e2905980ca77bf6
Ben Hubbard <55447225+humandad@users.noreply.github.com>
2024-06-28 12:08:55 -0400

    Load All sheets (#88)

--- apps/smartsheet/bundle/bots/load/sheets/bot.ts
+++ apps/smartsheet/bundle/bots/load/sheets/bot.ts
@@ -18,29 +18,12 @@ type SheetResponse = {
 }
 
 export default function smartsheet_load(bot: LoadBotApi) {
-	const { batchNumber = 0, batchSize, conditions } = bot.loadRequest
-	const queryParams = {} as Record<string, FieldValue>
-	const doPagination = typeof batchSize === "number" && batchSize > 0
-	if (doPagination) {
-		// SmartSheet pagination uses 1-based indexing
-		queryParams.page = batchNumber + 1
-		// Always add one to to the batch size to determine if there are more records
-		queryParams.pageSize = batchSize + 1
-	} else {
-		queryParams.includeAll = true
-	}
+	const { conditions } = bot.loadRequest
 
 	// Transform Conditions on the Uesio Core Id into row Id filters
 	const rowIdCondition = conditions?.find(
 		(condition) => condition.field === "uesio/core.id"
 	)
-	if (rowIdCondition) {
-		if (rowIdCondition.operator === "IN" && rowIdCondition.values?.length) {
-			queryParams.rowIds = rowIdCondition.values.join(",")
-		} else if (rowIdCondition.operator === "EQ" && rowIdCondition.value) {
-			queryParams.rowIds = rowIdCondition.value
-		}
-	}
 
 	let url = "https://api.smartsheet.com/2.0/sheets"
 	let isSingleSheet = false
@@ -53,6 +36,10 @@ export default function smartsheet_load(bot: LoadBotApi) {
 	if (rowIdCondition?.values) {
 		isSingleSheet = true
 		url = url + "/" + rowIdCondition.values[0] + "?rowNumbers=0"
+	}
+
+	if (!rowIdCondition) {
+		url = url + "?includeAll=true"
 	}
 
 	const response = bot.http.request({
